ff1e40d35ce43ed102f05192d52c68733da6b1b7
Muhammad Azhar <48376727+muazhar@users.noreply.github.com>
2020-09-12 23:58:34 +0500

    :alarm_clock: Create Event

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+});
+
+export const db = firebase.firestore()
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
